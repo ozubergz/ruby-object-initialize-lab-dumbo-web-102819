7f6c5560d8cfd7f036ccf55fff8e5e94debe0096
James Min <jmin499@gmail.com>
2019-10-29 16:29:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,13 @@
 require 'pry'
 
 class Dog
+  attr_reader :name
+  
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
   end
 end
+
+snoopy = Dog.new("Snoopy")
+snoopy.name
